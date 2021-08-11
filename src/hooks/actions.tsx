6b17961d8b07069bd161fb40801c8106801950ec
André Junior <andre.souza64@fatec.sp.gov.br>
2021-08-10 21:35:02 -0300

    Settings 1.7

--- src/hooks/actions.tsx
+++ src/hooks/actions.tsx
@@ -13,7 +13,7 @@ const ActionContextData = createContext({} as IActionContextData);
 
 export function ActionProvider({ children }: ActionProviderProps) {
   async function handleShare() {
-    const message = `Se liga galera o Aviva Jovem voltou!!!\nE você é o meu convidado\nSerá no 18 de Setembro às 19h00, na Sede Regional de Vargem Grande Pta., rua Nova Esperança, 64 - Jd. São Judas\n\nPara mais informações acesse o link: \nhttps://avivajovemadf.com`;
+    const message = `Se liga galera o Aviva Jovem voltou!!!\nE você é o meu convidado\nSerá no dia 18 de Setembro às 19h00, na Sede Regional de Vargem Grande Pta., rua Nova Esperança, 64 - Jd. São Judas\n\nPara mais informações acesse o link: \nhttps://avivajovemadf.com`;
 
     let url = '';
 
